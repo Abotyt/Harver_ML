bf85f111e3ca0214909a49113d103d1aa862888f
Abotyt <ytin0007@student.monash.edu>
2025-02-16 20:33:16 +1100

    Section 6 first attempt

---
+++ Section6_lab.R
@@ -0,0 +1,84 @@
+#Section 6
+#Pre-processing
+library(dslabs)
+mnist <- read_mnist()
+
+names(mnist)
+dim(mnist$train$images)
+
+class(mnist$train$labels)
+table(mnist$train$labels)
+
+# sample 10k rows from training set, 1k rows from test set
+set.seed(1990)
+index <- sample(nrow(mnist$train$images), 10000)
+x <- mnist$train$images[index,]
+y <- factor(mnist$train$labels[index])
+
+index <- sample(nrow(mnist$test$images), 1000)
+x_test <- mnist$test$images[index,]
+y_test <- factor(mnist$test$labels[index])
+
+library(matrixStats)
+library(ggplot2)
+sds <- colSds(x)
+qplot(sds, bins = 256, color = I("black"))
+
+library(caret)
+nzv <- nearZeroVar(x)
+image(matrix(1:784 %in% nzv, 28, 28))
+
+col_index <- setdiff(1:ncol(x), nzv)
+length(col_index)
+#------------------------------------------------------
+#KNN
+#set column names
+colnames(x) <- 1:ncol(mnist$train$images)
+colnames(x_test) <- colnames(x)
+
+#test with subset
+n <- 1000
+b <- 2
+index <- sample(nrow(x), n)
+control <- trainControl(method = "cv", number = b, p = .9)
+train_knn <- train(x[index ,col_index], y[index],
+                   method = "knn",
+                   tuneGrid = data.frame(k = c(3,5,7)),
+                   trControl = control)
+
+#cross validation with full dataset
+control <- trainControl(method = "cv", number = 10, p = .9)
+train_knn <- train(x[,col_index], y,
+                   method = "knn", 
+                   tuneGrid = data.frame(k = c(3,5,7)),
+                   trControl = control)
+
+#fit with final hyper parameter
+fit_knn <- knn3(x[ ,col_index], y,  k = 3)
+
+y_hat_knn <- predict(fit_knn,
+                     x_test[, col_index],
+                     type="class")
+cm <- confusionMatrix(y_hat_knn, factor(y_test))
+cm$overall["Accuracy"]
+
+cm$byClass[,1:2]
+
+#random forest
+library(randomForest)
+control <- trainControl(method="cv", number = 5)
+grid <- data.frame(mtry = c(1, 5, 10, 25, 50, 100))
+train_rf <-  train(x[, col_index], y,
+                   method = "rf",
+                   nTree = 150,
+                   trControl = control,
+                   tuneGrid = grid,
+                   nSamp = 5000)
+
+fit_rf <- randomForest(x[, col_index], y,
+                       minNode = train_rf$bestTune$mtry)
+
+y_hat_rf <- predict(fit_rf, x_test[ ,col_index])
+cm <- confusionMatrix(y_hat_rf, y_test)
+cm$overall["Accuracy"]
+
